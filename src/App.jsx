b60315c32ead8fb2ebc84ed0bf918c8c0bba99f3
ag7621 <aaron.c.guerra@gmail.com>
2025-03-03 18:09:39 -0700

    updated data, app
    added new boss entries, updated handleOnChange, added conditional rendering, changed input to textarea

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,89 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 const bosses = [
-  { name: 'sammy', entries: [] },
-  { name: 'sab', entries: [] },
-  { name: 'gor', entries: [] },
+  { name: 'Vale Guardian', entries: [] },
+  { name: 'Spirit Woods', entries: [] },
+  { name: 'Gorseval', entries: [] },
+  { name: 'Sabetha', entries: [] },
+  { name: 'Slothasor', entries: [] },
+  { name: 'Bandit Trio', entries: [] },
+  { name: 'Matthias', entries: [] },
+  { name: 'Escort', entries: [] },
+  { name: 'Keep Construct', entries: [] },
+  { name: 'Twisted Castle', entries: [] },
+  { name: 'Xera', entries: [] },
+  { name: 'Cairn', entries: [] },
+  { name: 'Mursaat Overseer', entries: [] },
+  { name: 'Samarog', entries: [] },
+  { name: 'Deimos', entries: [] },
+  { name: 'Soulless Horror', entries: [] },
+  { name: 'River of Souls', entries: [] },
+  { name: 'Statues of Grenth', entries: [] },
+  { name: 'Dhuum', entries: [] },
+  { name: 'Conjured Amalgamate', entries: [] },
+  { name: 'Twin Largos', entries: [] },
+  { name: 'Qadim', entries: [] },
+  { name: 'Gate of Ahdashim', entries: [] },
+  { name: 'Cardinal Adina', entries: [] },
+  { name: 'Cardinal Sabir', entries: [] },
+  { name: 'Qadim the Peerless', entries: [] },
 ];
 
 function App() {
   const [data, setData] = useState(bosses);
 
-  const [selectedBoss, setSelectedBoss] = useState('sammy');
+  const [selectedBoss, setSelectedBoss] = useState('Vale Guardian');
   const [newEntry, setNewEntry] = useState('');
+  const [count, setCount] = useState(0);
 
   function handleSubmit(e) {
     e.preventDefault();
 
     const newId = uuidv4();
-    console.log(newId);
-    console.log(selectedBoss);
-    console.log(newEntry);
+    const newEnter = { id: newId, entry: newEntry };
+
+    console.log(newEnter);
 
     setData((prevData) =>
       prevData.map((boss) =>
         boss.name === selectedBoss
           ? {
               ...boss,
-              entries: [...boss.entries, { id: newId, entry: newEntry }],
+              entries: [...boss.entries, newEnter],
             }
           : boss
       )
     );
+    setNewEntry('');
   }
 
-  useEffect(() => {
-    console.log('data change: ', data);
-    data.forEach((item) => {
-      console.log(item);
-    });
-  }, [data]);
+  function handleOnChange(e) {
+    setCount(e.target.value.length);
+    setNewEntry(e.target.value);
+  }
+
+  // useEffect(() => {
+  //   console.log('data change: ', data);
+  //   data.forEach((item) => {
+  //     console.log(item);
+  //   });
+  // }, [data]);
 
   return (
     <div>
       <h1>GW2 Comm Buddy 🦦</h1>
 
       <ul>
-        {data.map((boss) => (
-          <li key={boss.name}>
-            <h2>{boss.name}</h2>
-            {boss.entries.map((entry) => (
-              <p key={entry.id}>{entry.entry}</p>
-            ))}
-          </li>
-        ))}
+        {data.map((boss) =>
+          boss.entries.length > 0 ? (
+            <li key={boss.name}>
+              <h2>{boss.name}</h2>
+              {boss.entries.map((entry) => (
+                <p key={entry.id}>{entry.entry}</p>
+              ))}
+            </li>
+          ) : null
+        )}
       </ul>
 
       <hr />
@@ -71,15 +103,24 @@ function App() {
             </option>
           ))}
         </select>
-
+        <br />
         <label>Entry: </label>
-        <input
+        {/* <input
           type="text"
           value={newEntry}
           onChange={(e) => setNewEntry(e.target.value)}
+        /> */}
+        <textarea
+          rows={4}
+          cols={50}
+          maxLength={199}
+          value={newEntry}
+          onChange={handleOnChange}
         />
 
         <button>Add</button>
+
+        <p>{count}/199</p>
       </form>
     </div>
   );
